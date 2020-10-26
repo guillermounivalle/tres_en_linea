f1f1b0138a98c81a002069636aefab0bb8104ef2
guillermounivalle <guillermo.hernandez@correounivalle.edu.co>
2020-10-26 10:08:22 -0500

    The database requests have been created and the webpack and @babel packaging problem has been solved

--- src/server/routes/games.js
+++ src/server/routes/games.js
@@ -44,4 +44,36 @@ gamesRouter.route('/')
     res.end('PUT operation not supported on /Games');
 })
 
+
+gamesRouter.route('/:gameId')
+
+.options(cors.corsWithOptions, (req, res) => {
+    res.statusCode = 200
+})
+
+.get(cors.cors, (req,res, next) =>{
+    Games.findById(req.params.gameId)
+    .then((game) => {
+        res.statusCode = 200;
+        setHeader = ('Content-Type', 'application/json');
+        res.json(game);
+    }, (err) => next(err))
+    .catch((err) =>  next(err));
+})
+
+
+.put(cors.corsWithOptions, (req,res, next) =>{
+    Dishes.findByIdAndUpdate(req.params.gameId, {
+        $set: req.body
+    },{
+        new: true
+    })
+    .then((game) => {
+        res.statusCode = 200;
+        setHeader = ('Content-Type', 'application/json');
+        res.json(game);
+    }, (err) => next(err))
+    .catch((err) =>  next(err));
+})
+
 module.exports = gamesRouter;
