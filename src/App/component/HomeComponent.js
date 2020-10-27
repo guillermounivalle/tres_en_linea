e4428f00470756c6b3c1cdec604996a219df5515
guillermounivalle <guillermo.hernandez@correounivalle.edu.co>
2020-10-26 20:08:13 -0500

    update changes on HomePage

---
+++ src/App/component/HomeComponent.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+    
+    };
+
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row row-header" >
+                    <div class="col-sm"></div>
+                    <div class="col-sm">
+                        <h1>TRES EN RAYA</h1>
+                    </div>
+                    <div class="col-sm"></div>                                       
+                </div>
+                <div></div>
+            </div>
+        );
+    }
+}
+
+export default Home;
